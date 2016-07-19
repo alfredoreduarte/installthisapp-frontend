79019329757ce3a30777fe6c378905d21162aa78
Alfredo Re <alfredoreduarte@gmail.com>
2016-07-18 20:16:43 -0400

    Added trivia questions reducer

---
+++ modules/top_fans/reducers/questions.js
@@ -0,0 +1,12 @@
+import _ from 'lodash'
+
+const entities = (state = {}, action) => {
+	switch (action.type) {
+		case 'ADD_QUESTION':
+			return state
+		default:
+			return state
+	}
+}
+
+export default entities
