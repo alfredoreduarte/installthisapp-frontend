5160b2040e4b36d72d94d3c87f2928d8324bcbf2
Alfredo Re <alfredoreduarte@gmail.com>
2016-08-10 21:02:57 -0400

    Trivia canvas fully working

--- canvas/triviar/index.js
+++ canvas/triviar/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from 'react-dom'
 import { browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
-import Root from 'canvas/Trivia/containers/Root'
+import Root from 'canvas/trivia/containers/Root'
 import Root from 'containers/Root'
-import configureStore from 'canvas/Trivia/store/configureStore'
+import configureStore from 'canvas/trivia/store/configureStore'
 
 const store = configureStore()
 const history = syncHistoryWithStore(browserHistory, store)
