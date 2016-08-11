b9e23cf5f7b34950e81b18ee323b92b2cc9f69b4
Alfredo Re <alfredoreduarte@gmail.com>
2016-08-11 09:07:41 -0400

    Attempting to fix webpack build for production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -15,9 +15,9 @@ module.exports = {
 	},
 	plugins: [
 		new webpack.optimize.CommonsChunkPlugin('common.js'),
-		new webpack.optimize.DedupePlugin(),
-		new webpack.optimize.AggressiveMergingPlugin(),
-		new webpack.optimize.OccurenceOrderPlugin(),
+		// new webpack.optimize.DedupePlugin(),
+		// new webpack.optimize.AggressiveMergingPlugin(),
+		// new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production')
 		}),
