28511411c07b91e73f992ba0b65b6f7627f9f5ca
Alfredo Re <alfredoreduarte@gmail.com>
2016-08-24 22:18:44 -0400

    Using large photos from facebook

--- components/FbPhoto.js
+++ components/FbPhoto.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react'
 
 const FbPhoto = ({ className, identifier }) => (
-	<img className={className} src={`https://graph.facebook.com/${identifier}/picture?type=square`} />
+	<img className={className} src={`https://graph.facebook.com/${identifier}/picture?type=large`} />
 )
 
 export default FbPhoto
