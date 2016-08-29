50316f43e9a8d55f5a1a3f124a6a6e68e68f35ee
Alfredo Re <alfredoreduarte@gmail.com>
2016-08-29 10:15:50 -0400

    Testing env variables for production

--- server.js
+++ server.js
@@ -63,6 +63,9 @@ app.use(function(req, res, next) {
 })
 app.get('/', function(req, res) {
 	res.render('landing', {
+		env: process.env.NODE_ENV,
+		apiiurl: process.env.API_URL,
+		fbappid: process.env.FB_APP_ID,
 		apiUrl,
 		facebookAppId,
 	})
