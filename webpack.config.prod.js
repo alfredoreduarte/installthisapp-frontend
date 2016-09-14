e1efcaba616b4d9c8306a083a1e1c6ccce5f486f
Alfredo Re <alfredoreduarte@gmail.com>
2016-09-14 04:57:03 -0400

    Added production image loader

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -45,6 +45,12 @@ module.exports = {
 				exclude: /node_modules/,
 				include: __dirname
 			},
+			// images
+			{
+				test: /\.(png|jpg|jpeg|gif)$/,
+				// include: path.join(__dirname, 'assets/styles'),
+				loaders: ['file']
+			},
 			// sass
 			{
 				test: /\.(sass|scss)$/,
