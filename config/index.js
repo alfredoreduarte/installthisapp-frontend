6ba503fb483591b5b8c1631e3d6d5765a724b075
Alfredo Re <alfredoreduarte@gmail.com>
2016-09-20 11:15:26 -0400

    Added settings for heroku CLI app

--- config/index.js
+++ config/index.js
@@ -1,18 +1,1 @@
-// process.env.PORT = process.env.PORT || 4000
-// process.env.HOST = process.env.HOST || 'installthisapp.local'
-
-// const getUrl = () => {
-// 	switch (process.env.NODE_ENV){
-// 		case 'test':
-// 			return 'http://installthisapp.local:4000'
-// 		case 'development':
-// 			return 'https://local.installthisapp.com'
-// 		case 'production':
-// 			return 'https://stage.installthisapp.com'
-// 		default:
-// 			return 'http://' + process.env.HOST + ':' + process.env.PORT
-// 	}
-// }
-
-// export const API_URL = process.env.API_URL
 export const API_URL = window.apiUrl
