0d2affc49ab8502ce934f61884025bc2605eccee
Alfredo Re <alfredoreduarte@gmail.com>
2016-12-14 06:32:06 -0300

    Showing link to billing page

--- components/AccountSidebar.js
+++ components/AccountSidebar.js
@@ -5,7 +5,7 @@ const AccountSidebar = () => (
 	<div className="col-md-2 col-xs-12 ita-main-menu">
 		<ul className="list-unstyled">
 			<li><IndexLink to="/d/account" activeClassName="active">Preferences</IndexLink></li>
-			<li className="hide"><Link to="/d/account/billing" activeClassName="active">Billing</Link></li>
+			<li><Link to="/d/account/billing" activeClassName="active">Billing</Link></li>
 			<li><Link to="/d/account/team" activeClassName="active">Team</Link></li>
 			<li><Link to="/d/account/referrals" activeClassName="active">Referrals</Link></li>
 		</ul>
