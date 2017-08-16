17a4c81bdfa1f1a739f5aa39ec0a42fefe419715
Alfredo Re <alfredoreduarte@gmail.com>
2017-08-16 13:13:54 -0400

    Fixed new navbar to reflect admin's subscription plan

--- components/AppNavBar.js
+++ components/AppNavBar.js
@@ -1,87 +1,50 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import moment from 'moment'
-import { Navbar, NavDropdown, MenuItem } from 'react-bootstrap'
-import { Link, IndexLink } from 'react-router'
-import { LinkContainer } from 'react-router-bootstrap'
-import { push } from 'react-router-redux'
+import { NavDropdown, MenuItem } from 'react-bootstrap'
+import { IndexLink } from 'react-router'
 import { logOut } from 'actions/admin'
 import FbPhoto from 'components/FbPhoto'
 import StatusIndicator from 'components/StatusIndicator'
 
 const AppNavBar = ({ adminId, name, identifier, logout, subscription, remainingTrialDays, showLeadgen }) => (
-	<div style={{
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-		// padding: '0px 20px',
-		height: '70px',
-		background: 'white',
-		boxShadow: '0px 2px 5px rgba(0,0,0,.05)',
-		marginBottom: '40px',
-	}}>
-		
-		<IndexLink to="/d" style={{
-			padding: '0px 20px',
-			display: 'flex',
-			borderRight: '1px solid #e6e6e6',
-			alignSelf: 'center',
-			height: '100%',
-			alignItems: 'center',
-		}}>
+	<div className="AppNavBar">
+		<IndexLink className="AppNavBar-IndexLink" to="/d">
 			<img src="/images/logo-v3.png" style={{height: "28px"}} />
 		</IndexLink>
-
-		<div className="" style={{
-			flexGrow: 1,
-			display: 'flex',
-			justifyContent: 'space-between',
-			padding: '0px 20px',
-		}}>
-			<ul className="list-unstyled" style={{
-				display: 'flex',
-				alignItems: 'center',
-				marginBottom: '0px',
-			}}>
-				{!showLeadgen ? <li><a href='/d'>← Back to contests</a></li> : null}
-				{showLeadgen ? <li>
-					<a href='/leadgen' className="btn btn-primary btn-sm animated rubberBand" style={{letterSpacing: '1px'}}>
-						<b>NEW!</b> Facebook Lead Ads sync
-					</a></li> : null}
+		<div className="AppNavBar-MenuBar">
+			<ul className="list-unstyled AppNavBar-MenuList">
+				{!showLeadgen ? 
+					<li><a href='/d'>← Back to contests</a></li>
+					 : 
+					<li>
+						<a href='/leadgen' 
+							className="btn btn-primary btn-sm animated rubberBand" 
+							style={{letterSpacing: '1px'}}>
+							<b>NEW!</b> Facebook Lead Ads sync
+						</a>
+					</li>}
 			</ul>
-			<ul className="nav navbar-nav navbar-right" style={{
-				display: 'flex',
-				alignItems: 'center',
-				marginBottom: '0px',
-			}}>
-				<li>
+			<ul className="nav navbar-nav navbar-right AppNavBar-MenuList">
+				<li className={subscription ? null : 'hide'}>
 					<a href='?offer=trial-ended' className="link-no-underline text-success">
 						<small>{remainingTrialDays} | UPGRADE</small>
 					</a>
 				</li>
-				<NavDropdown 
-					eventKey={3} 
+				<NavDropdown eventKey={3} id="account-dropdown"
 					title={
-						identifier ? <FbPhoto 
-							identifier={parseInt(identifier)} 
-							width={42} 
-							height={42} 
-							className="img-circle"
-						/>
-						: <img
-							src={`/images/user-placeholders/${adminId % 8}.png`}
-							width="42px"
-							height="42px"
-						/>
-					} 
-					id="account-dropdown">
-					
+						identifier ? <FbPhoto identifier={parseInt(identifier)} width={42} height={42} className="img-circle" />
+						: <img src={`/images/user-placeholders/${adminId % 8}.png`} width="42px" height="42px" />
+					}>
 						<MenuItem eventKey={3.3} href="/d/account">
 							<span>My Account
-							{name ? <span></span> : <span style={{marginLeft: '5px'}}><StatusIndicator active={true} status={'red'} /></span>}
+								{name ? <span></span> 
+									: 
+								<span style={{marginLeft: '5px'}}>
+									<StatusIndicator active={true} status={'red'} />
+								</span>}
 							</span>
 						</MenuItem>
-					
 					<MenuItem eventKey={3.2} href="javascript:void(0)" onClick={logout}>Logout</MenuItem>
 				</NavDropdown>
 			</ul>
