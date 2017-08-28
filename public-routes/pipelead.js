3d3daf5173ce84e12d197b615b2ef0e2ab71598c
Alfredo Re <alfredoreduarte@gmail.com>
2017-08-28 12:59:16 -0400

    Added PipeLead.co landing page

--- public-routes/pipelead.js
+++ public-routes/pipelead.js
@@ -1,35 +1,8 @@
 const express = require('express')
-const _ = require('lodash')
-const jsonfile = require('jsonfile')
-
-const cloudFrontUrl = process.env.CLOUDFRONT_URL
 
 const router = express.Router()
-const manifestPath = `${process.cwd()}/webpack-assets.json`
-const manifest = jsonfile.readFileSync(manifestPath)
-const manifestBundle = manifest['manifest']['js']
-const jsBundle = manifest['landing']['js']
-const cssBundle = manifest['landing']['css']
-const vendorBundle = manifest['common']['js']
-const isOldUser = false
-
-const commonParams = req => {
-	return {
-		canonical: false,
-		req: req,
-		disableSegment: req.query[process.env.ALIAS_PARAM_KEY] || process.env.NODE_ENV == 'development',
-		cloudFrontUrl: cloudFrontUrl,
-		apiUrl: process.env.API_URL,
-		facebookAppId: process.env.FB_APP_ID,
-		jsBundle,
-		cssBundle,
-		vendorBundle,
-		manifestBundle,
-		isOldUser,
-	}
-}
 
 /* GET home page. */
-router.get('/', function(req, res) { res.render('landing-v2/apps', commonParams(req)) })
+router.get('/', function(req, res) { res.render('pipelead/index') })
 
 module.exports = router
--- server.js
+++ server.js
@@ -46,7 +46,8 @@ else {
 }
 
 var pipeleadRoutes = require('./public-routes/pipelead')
-app.use(vhost('www.pipelead.co', pipeleadRoutes))
+app.use(vhost('www.pipelead.*', pipeleadRoutes))
+app.use(vhost('pipelead.*', pipeleadRoutes))
 
 // Images and other static asssets
 app.use('/images', express.static(__dirname + '/assets/images'))
@@ -105,22 +106,22 @@ var s3 = require('./s3')
 app.get('/sign-s3', s3)
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-// 	var err = new Error('Not Found')
-// 	err.status = 404
-// 	next(err)
-// })
+app.use(function(req, res, next) {
+	var err = new Error('Not Found')
+	err.status = 404
+	next(err)
+})
 
 // error handler
-// app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
 // 	// set locals, only providing error in development
-// 	res.locals.message = err.message
-// 	res.locals.error = req.app.get('env') === 'development' ? err : {}
+	res.locals.message = err.message
+	res.locals.error = req.app.get('env') === 'development' ? err : {}
 
 // 	// render the error page
-// 	res.status(err.status || 500)
-// 	res.render('error')
-// })
+	res.status(err.status || 500)
+	res.render('error')
+})
 
 // 
 // === Sitemap & robots.txt ===
