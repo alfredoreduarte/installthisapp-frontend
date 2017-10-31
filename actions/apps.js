e5591c1b712cf867de4e8a21c8ce862f78051347
Alfredo Re <alfredoreduarte@gmail.com>
2017-10-31 18:09:42 -0300

    Added Capture the Flag to post-creation special redirect

--- actions/apps.js
+++ actions/apps.js
@@ -200,6 +200,7 @@ export const postNewApp = () => {
 						body.module == 'fan_gate' ||
 						body.module == 'coupons' ||
 						body.module == 'photo_contest' ||
+						body.module == 'capture_the_flag' ||
 						body.module == 'static_html'
 					) {
 						dispatch(push(`/d/apps/${response.app.applicationType}/${response.app.checksum}/editor`))
