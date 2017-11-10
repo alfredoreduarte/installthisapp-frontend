57ea85f0e90034fbe26c2fda3bf2abb041f9cf82
Alfredo Re <alfredoreduarte@gmail.com>
2017-11-10 17:58:35 -0300

    Showing only positive or zero on timer

--- canvas/capture_the_flag/selectors/timer.js
+++ canvas/capture_the_flag/selectors/timer.js
@@ -1,5 +1,9 @@
 import toHHMMSS from 'lib/toHHMMSS'
 
 export const getTimer = state => {
-	return toHHMMSS(state.entities.timeLeft)
+	if (state.entities.timeLeft > 0) {
+		return toHHMMSS(state.entities.timeLeft)
+	} else {
+		return '00:00:00'
+	}
 }
